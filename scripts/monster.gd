3082f6d8425df6d9d1de3f8102b4e845d07ffef7
aliaprince916-commits <aliaprince916@gmail.com>
2026-03-15 17:59:32 +0100

    monster

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -1,24 +1,47 @@
 extends CharacterBody2D
 
-
-@onready var direcror: RayCast2D = $direcror
+@onready var director: RayCast2D = $director
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 # هذ المتغير لازم يكون في كل حاجة حاب نبدل لها الجاذبية
 var dir_g=1
 var dir_m=1
-const  SPEED=300
+# متغيرا استطاعة الاستدار
+var can_turn=false
+const  SPEED=50
 func _physics_process(delta: float) -> void:
 	# معلومات مهمة حول متغير انجاه الجاذبية والبحث عن حوله في الارض او السقف مع الجاذبية صحيحة
 	var is_floor=is_grav(dir_g)
 	# جاذبية 
 	if not is_floor:
 		velocity+=get_gravity()*delta*dir_g
+	# حركة االوحش يمين ويسار
 	velocity.x=SPEED*dir_m
+	#تغير اتجاه الراي كاست بحسب المتغير التجاه
+	if dir_m==1:
+		animated_sprite_2d.flip_h=false
+		director.target_position.x=8
+	else:
+		animated_sprite_2d.flip_h=true
+		director.target_position.x=-8
+	# تحديد الوضع جاذبية فوق او تحت على الانميشن
+	if dir_g==1:
+		animated_sprite_2d.flip_v=false
+		director.target_position.y=18
+	else:
+		animated_sprite_2d.flip_v=true
+		director.target_position.y=-18
+	if dir_m>0:
+		animated_sprite_2d.play("walk")
+	move(is_floor)
 	move_and_slide()
 
-func move(delta):
-	if !direcror.is_colliding() or is_on_wall():
-		dir_m*=-1
+func move(is_floor):
+	if is_floor:
+		if (is_on_wall() or !director.is_colliding()) and can_turn:
+			dir_m *= -1
+			can_turn = false
+		if !is_on_wall():
+			can_turn = true
 	pass
 # البحث عن هل هو في الجاذبية الصحيحة لتصحيح التجاه
 func is_grav(g):
